cc455f5997110982f3cf7922f70a87c0041cf711
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-12-15 00:19:08 -0500

    delete call working

--- client/favlinks/src/components/LinkContainer.jsx
+++ client/favlinks/src/components/LinkContainer.jsx
@@ -1,55 +1,84 @@
-import { useState } from 'react'
-import Table from './Table'
-import Form from './Form'
+import { useEffect, useState } from "react";
+import Table from "./Table";
+import Form from "./Form";
 
-function LinkContainer(){
-    const [favLinks, setFavLinks] = useState([]);
+function LinkContainer() {
+  const [favLinks, setFavLinks] = useState([]);
+  const [detectChange, setDetectChange] = useState(false);
 
-    const handleRemove = (index) => {
-        /*
-            TODO - Create logic for setting the state to filter array and remove favLink at index
-        */
-        const updatedLinks = [...favLinks];
-        updatedLinks.splice(index, 1);
-        setFavLinks(updatedLinks);
-      }
-    
-      const handleSubmit = async (favLink) => {
-        // if (!Array.isArray(favLinks)) {
-        //   setFavLinks([favLink]);
-        // } else {
-        //   setFavLinks([...favLinks, favLink]);
-        // }
-        try {
-          const response = await fetch("http://localhost:3000/api/links", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify(favLink)
-          });
-    
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
+  useEffect(() => {
+    const getLinks = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/links", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-          console.log(response)
-    
-        } catch (error) {
-          console.error("Error:", error);
-          console.log("There has been a error creating a link")
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
-      };
 
-    return(
-        <div>
-            <h1>My Favorite Links</h1>
-            <p>Add a new link with a name and URL to the table! </p>
-            <Table linkData={favLinks} removeLink={handleRemove}/>
-            <h1>Add New</h1>
-            <Form addLink={handleSubmit}/>
-        </div>
-    )
+        const data = await response.json();
 
+        setFavLinks(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error:", error);
+        console.log("There has been a error getting the links");
+      }
+    };
+
+    getLinks();
+  }, [detectChange]);
+
+  const handleRemove = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/links/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      // setFavLinks(favLinks.filter((link) => link.id !== id));
+      // setDetectChange(!detectChange);
+    } catch (error) {
+      console.error("Error deleting favLink:", error);
+    }
+  };
+  
+
+  const handleSubmit = async (favLink) => {
+    try {
+      const response = await fetch("http://localhost:3000/api/links", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(favLink),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      console.log(response);
+      setDetectChange(!detectChange);
+    } catch (error) {
+      console.error("Error:", error);
+      console.log("There has been a error creating a link");
+    }
+  };
+
+  return (
+    <div>
+      <h1>My Favorite Links</h1>
+      <p>Add a new link with a name and URL to the table! </p>
+      <Table linkData={favLinks} removeLink={handleRemove} />
+      <h1>Add New</h1>
+      <Form addLink={handleSubmit} />
+    </div>
+  );
 }
 export default LinkContainer;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const db = require("./db");
-const cors = require("cors")
+const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(morgan("tiny"));
 
 const PORT = 3000;
 
