36431d5f19f35328e37fb33f4e8dba64fce18c8c
Luis Molinuevo <luismolinuevo0@gmail.com>
2023-12-14 22:34:39 -0500

    get links route created

--- server/db.js
+++ server/db.js
@@ -1,8 +1,21 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'favlinks',
-  password: 'password',
+  user: "postgres",
+  host: "localhost",
+  database: "favlinks",
+  password: "password",
   port: 5432,
-})
+});
+
+const getLinks = (req, res) => {
+  pool.query("SELECT * FROM favlinks ORDER BY id ASC", (error, result) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(result.rows);
+  });
+};
+
+module.exports = {
+  getLinks,
+};
